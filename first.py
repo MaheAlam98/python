e2dd3499d44b246260015f6a0b3a2056325b7122
MaheAlam98 <mdmahealam98@gmail.com>
2024-02-26 23:06:00 +0600

    user input

--- first.py
+++ first.py
@@ -1,10 +1,53 @@
-#basic pythons learning
+# variable
+value=10
+b="value"
+print(b)
 
-"""
-print("tu\nhin")
-"""
-'''
-print(5*8)
-'''
-print("9/8")
-print('tuhin')
+# variable type
+a=10
+b=True
+c="Tuhin"
+d=102.5
+e=complex(4,2)
+print(e)
+print("type of a is:" ,type(a))
+print("type of b is:" ,type(b))
+print("type of c is:" ,type(c))
+print("type of d is:" ,type(d))
+print("type of e is:" ,type(e))
+
+# operator
+print (15 + 5)  #addition
+print (15 - 5)  #subtraction
+print (15 * 5)  #multiplication
+print (15 ** 5) #power (15^5)/exponential
+print (15 / 5)  #divide with floating number
+print (15 // 5) #divide without floating number
+print (15 % 5)  #modulo(Bag ses)
+
+# make a calculator
+
+a=20
+b=30
+
+print ("the value of:", a ,"+", b,"=",a+b)
+print ("the value of:", a ,"*", b,"=",a*b)
+print ("the value of:", a ,"-", b,"=",a-b)
+print ("the value of:", a ,"/", b,"=",a/b)
+
+# type casting
+a=10.3
+b=4
+c=a+b
+print("Type of a:",type(a))
+print("Type of b:",type(b))
+print("Type of c:",type(c))
+
+# user input
+
+x=input("Enter the first number:")
+y=input("Enter the second number:")
+
+
+print(x+y)
+print(int(x)+int(y))
