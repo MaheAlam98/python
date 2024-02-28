efe90350ca0478f438ccb4bbdb564e287486178d
MaheAlam98 <mdmahealam98@gmail.com>
2024-02-29 01:00:40 +0600

    For loops

---
+++ loops.py
@@ -0,0 +1,28 @@
+#print string using for loops
+name="Mahe-Alam"
+for i in name:
+    print(i)
+
+# print element in a tuple using for loops
+x={"kamal","jamal","hanif","alif","shihab","tuhin"}
+for j in x:
+    print(j)
+
+# print tuple element in sigle 
+y={"kamal","jamal","hanif","alif","shihab","tuhin"}
+for j in x:
+    print(j)
+    for p in j:
+        print(p)
+
+# print natural number using loops 
+for j in range(100):
+    print(j)
+
+# print number in a given range using loops
+for j in range(10,20):
+    print(j)
+
+# print number in a given range increment by 5 in each step
+for j in range(10,50,5):
+    print(j)
