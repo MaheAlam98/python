8682357d5716dd26fc5470f12bf7fd49b91786ab
MaheAlam98 <mdmahealam98@gmail.com>
2024-02-29 19:21:00 +0600

    break and continue

---
+++ breakAndcontinue.py
@@ -0,0 +1,11 @@
+# Break statement
+for i in range(20):
+    print(i)
+    if(i==10):
+        break  #when i==10 the loop is terminated/stop
+
+# Continue statement
+for i in range(20): 
+    print(i)
+    if(i==10):
+        continue #when i==10 the the loop is skip the iteration   
