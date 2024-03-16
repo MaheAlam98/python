e0926ece08fefc636576900b234bfd3a6124d0c4
MaheAlam98 <mdmahealam98@gmail.com>
2024-03-16 16:30:28 +0600

    import

---
+++ im.py
@@ -0,0 +1,5 @@
+from importTest import *
+rating(50)
+rating(20)
+rating(80)
+rating(66)
---
+++ importTest.py
@@ -0,0 +1,7 @@
+def rating(mark):
+    if(mark>=80):
+        print("good")
+    elif(mark >=50 and mark <=79):
+        print("Average ")
+    else:
+        print("Very Bad")
