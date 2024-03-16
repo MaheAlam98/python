e4d5650dd1273c5179342c6f9417964601351988
MaheAlam98 <mdmahealam98@gmail.com>
2024-03-16 23:51:01 +0600

    OS Module

---
+++ os_module.py
@@ -0,0 +1,9 @@
+import os
+# if(not os.path.exists()):
+#     os.mkdir("tuhin")
+# for i in range(0, 10):
+#     os.mkdir(f"tuhin/day {i+1}")
+
+
+for i in range(0, 10):
+    os.rename(f"tuhin/day {i+1}",f"tuhin/tutorial {i+1}")
